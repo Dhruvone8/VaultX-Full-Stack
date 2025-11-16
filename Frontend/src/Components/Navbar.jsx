1237027c2e223de888f53cbb48b49c820a6391ab
Dhruv Singhania <dhruv.s0611@gmail.com>
2025-11-16 21:43:07 +0530

    Added Github Img

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
+import github from '../assets/github.png'
 
 const Navbar = () => {
     return (
         <nav className='bg-indigo-800 text-white'>
             <div className="flex justify-between px-4 py-3 h-auto items-center">
+
                 <div className="logo font-bold text-white text-2xl mx-3">
                     <span className='text-cyan-400'>&lt;</span>
                     VaultX
                     <span className='text-cyan-400'>/&gt;</span>
                 </div>
+
                 <button className='mx-2'>
-                    <img className="cursor-pointer hover:scale-110 transition-all duration-300 invert w-12 mx-2" 
-                    src="src/assets/github.png" alt="github" />
+                    <a 
+                        href="https://github.com/Dhruvone8/VaultX-Full-Stack" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        <img 
+                            className="cursor-pointer hover:scale-110 transition-all duration-300 invert w-12 mx-2" 
+                            src={github} 
+                            alt="github" 
+                        />
+                    </a>
                 </button>
             </div>
         </nav>
